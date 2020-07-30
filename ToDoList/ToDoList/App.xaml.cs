0631555253b2091c48a833f7d682c89720e6eb8c
ericjai <59076768+ericjai@users.noreply.github.com>
2020-07-30 14:47:15 -0700

    Updated

---
+++ ToDoList/ToDoList/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ToDoList
+{
+    public partial class App : Application
+    {
+
+        public static string FolderPath { get; set; }
+        public App()
+        {
+            InitializeComponent();
+            FolderPath =Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+            MainPage = new NavigationPage(new NotesPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ ToDoList/ToDoList/NoteEntryPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoList.Model;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ToDoList
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NoteEntryPage : ContentPage
+    {
+        public NoteEntryPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnSaveButtonClicked(Object Sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+            if (string.IsNullOrWhiteSpace(note.Filename))
+            {
+                var filename=Path.Combine(App.FolderPath, $"{Path.GetRandomFileName()}.notes.txt");
+
+                File.WriteAllText(filename, note.Text);
+            }
+            else
+            {   
+                File.WriteAllText(note.Filename, note.Text);
+            }
+            await Navigation.PopAsync();
+
+        }
+
+        async void OnDeleteButtonClicked(Object Sender, EventArgs e)
+        {
+            var note = BindingContext as Note;  // or BindingContext as Note (Note)BindingContext
+
+            if (note == null)
+                return;
+            if (File.Exists(note.Filename))
+            {
+                File.Delete(note.Filename);
+
+            }
+            await Navigation.PopAsync();
+
+           
+
+
+        }
+    }
+}
